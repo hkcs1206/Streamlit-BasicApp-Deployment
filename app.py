4f9d7a089e493ba5cda063c700eeb07da270cc3b
hkcs1206 <96352622+hkcs1206@users.noreply.github.com>
2022-11-29 21:24:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.write('Substraction of Two Numbers')
-st.head("Enter the Two Numbers")
+st.header("Enter the Two Numbers")
 number1 = st.number_input("Enter the 1st number")
 number2 = st.number_input("Enter the 2st number")
 sub = number1 - number2
